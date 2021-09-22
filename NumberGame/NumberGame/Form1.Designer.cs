62c46773aed2a7f223e6e3acff08bf3be805bb0d
joppofiss <59013738+joppofiss@users.noreply.github.com>
2021-09-22 11:16:35 +0300

    C# Windows Form Application
    C# Windows Form Application

---
+++ NumberGame/NumberGame/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace NumberGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(328, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Please enter a 4 digit number";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(56, 51);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Player plays";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(56, 81);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(134, 24);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Computer plays";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleDescription = "";
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox2.Location = new System.Drawing.Point(386, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TabStop = false;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(469, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(333, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 27);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(386, 241);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 27);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(254, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Computer\'s Guess";
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleDescription = "";
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.textBox4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox4.Location = new System.Drawing.Point(386, 160);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(77, 27);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ NumberGame/NumberGame/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//form2 = new Form2();
+//form2.ShowDialog();
+
+namespace NumberGame
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2;
+        int count = 0;
+        int plusMinusCount = 0;
+        int currentValue = 0;
+        int oldValue = 0;
+        int firstValue = 0;
+        int computerGuessNumber;
+        int playerGuessNumber;
+        int suggestedNumber;
+        int computerNumber = randomNumberGenerator();
+
+        public class cowsAndBullsGame
+        {
+            int guessNumber;
+            public cowsAndBullsGame(int number)
+            {
+                guessNumber = number;
+
+            }
+            public void digitCompare(int computerNumber, ref int countCows, ref int countBulls)
+            {
+                int guessDigit1 = 0;
+                int guessDigit2 = 0;
+                int guessDigit3 = 0;
+                int guessDigit4 = 0;
+                int flagDigit1 = 0;
+                int flagDigit2 = 0;
+                int flagDigit3 = 0;
+                int flagDigit4 = 0;
+
+                int computerDigit1 = (computerNumber / 1000) % 10;
+                int computerDigit2 = (computerNumber / 100) % 10;
+                int computerDigit3 = (computerNumber / 10) % 10;
+                int computerDigit4 = (computerNumber / 1) % 10;
+
+                guessDigit1 = (guessNumber / 1000) % 10;
+                guessDigit2 = (guessNumber / 100) % 10;
+                guessDigit3 = (guessNumber / 10) % 10;
+                guessDigit4 = (guessNumber / 1) % 10;
+
+                int digitToCheck1 = computerDigit1;
+                int digitToCheck2 = computerDigit2;
+                int digitToCheck3 = computerDigit3;
+                int digitToCheck4 = computerDigit4;
+                for (int i = 0; i <= 9; i++)
+                {
+                    if (flagDigit1 == 0)
+                    {
+                        if (guessDigit1 == digitToCheck1)
+                        {
+                            countBulls++;
+                            flagDigit1 = 1;
+                        }
+                        else if (guessDigit1 == digitToCheck2)
+                        {
+                            countCows--;
+                            flagDigit1 = 1;
+                        }
+                        else if (guessDigit1 == digitToCheck3)
+                        {
+                            countCows--;
+                            flagDigit1 = 1;
+                        }
+                        else if (guessDigit1 == digitToCheck4)
+                        {
+                            countCows--;
+                            flagDigit1 = 1;
+                        }
+                    }
+
+                    if (flagDigit2 == 0)
+                    {
+                        if (guessDigit2 == digitToCheck2)
+                        {
+                            countBulls++;
+                            flagDigit2 = 1;
+                        }
+                        else if (guessDigit2 == digitToCheck1)
+                        {
+                            countCows--;
+                            flagDigit2 = 1;
+                        }
+                        else if (guessDigit2 == digitToCheck3)
+                        {
+                            countCows--;
+                            flagDigit2 = 1;
+                        }
+                        else if (guessDigit2 == digitToCheck4)
+                        {
+                            countCows--;
+                            flagDigit2 = 1;
+                        }
+                    }
+
+                    if (flagDigit3 == 0)
+                    {
+
+                        if (guessDigit3 == digitToCheck3)
+                        {
+                            countBulls++;
+                            flagDigit3 = 1;
+                        }
+                        else if (guessDigit3 == digitToCheck1)
+                        {
+                            countCows--;
+                            flagDigit3 = 1;
+                        }
+                        else if (guessDigit3 == digitToCheck2)
+                        {
+                            countCows--;
+                            flagDigit3 = 1;
+                        }
+                        else if (guessDigit3 == digitToCheck4)
+                        {
+                            countCows--;
+                            flagDigit3 = 1;
+                        }
+                    }
+
+                    if (flagDigit4 == 0)
+                    {
+                        if (guessDigit4 == digitToCheck4)
+                        {
+                            countBulls++;
+                            flagDigit4 = 1;
+                        }
+                        else if (guessDigit4 == digitToCheck1)
+                        {
+                            countCows--;
+                            flagDigit4 = 1;
+                        }
+                        else if (guessDigit4 == digitToCheck2)
+                        {
+                            countCows--;
+                            flagDigit4 = 1;
+                        }
+                        else if (guessDigit4 == digitToCheck3)
+                        {
+                            countCows--;
+                            flagDigit4 = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        bool IsUnique(string text) //checks if input number digits are unique
+        {
+            if (text[0] == text[1] || text[0] == text[2] || text[0] == text[3] ||
+                text[1] == text[2] || text[1] == text[3] || text[2] == text[3])
+                return false;
+            else
+                return true;
+        }
+
+        static int randomNumberGenerator() //generates a random number with unique digits
+        {
+            int min = 1000;
+            int max = 9999;
+            Random number = new Random();
+
+            int generatedDigit1 = 0;
+            int generatedDigit2 = 0;
+            int generatedDigit3 = 0;
+            int generatedDigit4 = 0;
+
+            int generatedNumber = 0;
+
+            while (generatedDigit1 == generatedDigit2 || generatedDigit1 == generatedDigit3 || generatedDigit1 == generatedDigit4 ||
+                   generatedDigit2 == generatedDigit3 || generatedDigit2 == generatedDigit4 || generatedDigit3 == generatedDigit4)
+            {
+                generatedNumber = number.Next(min, max);
+                generatedDigit1 = (generatedNumber / 1000) % 10;
+                generatedDigit2 = (generatedNumber / 100) % 10;
+                generatedDigit3 = (generatedNumber / 10) % 10;
+                generatedDigit4 = (generatedNumber / 1) % 10;
+                
+            }
+
+            return generatedNumber;
+        }
+
+        bool plusMinusCheck(string x)
+        {
+            var plusminuscheck = new[] { "+", "-" };
+            bool value = plusminuscheck.Any(x.Contains);
+            return value;
+        }
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+            radioButton1.Checked = true;
+            textBox1.Text = "Guess the 4-digit number";
+            textBox2.Text = "0";
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            label1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            string numberToGuess = randomNumberGenerator().ToString();
+
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                textBox1.Text = "Guess the 4-digit number";
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                label1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                string numberToGuess = randomNumberGenerator().ToString();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                textBox1.Text = "Enter a 4-digit number";
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+
+            }
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked == true && textBox1.Text.Length == 0)
+                textBox1.Text = "Guess the 4-digit number";
+            else if (radioButton2.Checked == true && textBox1.Text.Length == 0)
+                textBox1.Text = "Enter a 4-digit number";
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 4)
+            {
+                MessageBox.Show("Please enter 4 digits");
+                textBox1.Text = "";
+            }
+            if (radioButton2.Checked == true && textBox1.TextLength == 4)
+            {
+                
+                if (IsUnique(textBox1.Text) == false)
+                {
+                    MessageBox.Show("Please enter unique digits");
+                }
+
+                else if (textBox1.Text.StartsWith("0") == true)
+                {
+                    MessageBox.Show("Number cannot start with 0");
+                    textBox1.Text = "";
+                }
+                else
+                {
+                    count++;
+                    textBox2.Visible = true;
+                    textBox3.Visible = true;
+                    textBox4.Visible = false;
+                    label1.Visible = true;
+                    button2.Visible = true;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                    computerGuessNumber = int.Parse(textBox1.Text);
+                }
+            }
+
+            if (radioButton1.Checked == true && textBox1.TextLength == 4)
+            {
+                if (IsUnique(textBox1.Text) == false)
+                {
+                    MessageBox.Show("Please enter unique digits");
+                }
+                else if (textBox1.TextLength != 4)
+                {
+                    MessageBox.Show("Please enter 4 digits");
+                    textBox1.Text = "";
+                }
+                else if (textBox1.Text.StartsWith("0") == true)
+                {
+                    MessageBox.Show("Number cannot start with 0");
+                    textBox1.Text = "";
+                }
+                else
+                {
+                    playerGuessNumber = int.Parse(textBox1.Text);
+                    textBox4.Visible = true;
+                    cowsAndBullsGame Game1 = new cowsAndBullsGame(playerGuessNumber);
+                    int cows=0;
+                    int bulls=0;
+                    
+                    Game1.digitCompare(computerNumber, ref cows, ref bulls);
+                    textBox4.Text = computerNumber.ToString();
+                    if (cows != 0 && bulls != 0)
+                    {
+                        MessageBox.Show(cows.ToString());
+                        MessageBox.Show("+" + bulls.ToString());
+                    }
+                    else if (cows == 0 && bulls != 0)
+                    {
+                        MessageBox.Show("+" + bulls.ToString());
+                        if (bulls == 4)
+                            MessageBox.Show("Correct");
+                    }
+                    else if (cows != 0 && bulls == 0)
+                        MessageBox.Show(cows.ToString());
+                    else
+                        MessageBox.Show("0");
+                }
+            }
+
+        }       
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsSymbol(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+            while (plusMinusCount <= 2)
+            {
+                oldValue = currentValue;
+                firstValue = oldValue;
+                plusMinusCount++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentValue = int.Parse(textBox2.Text);
+            if (currentValue < 4)
+            {
+                currentValue++;
+            }
+            textBox2.Text = currentValue.ToString();           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentValue = int.Parse(textBox2.Text);
+            if (currentValue > -4)
+            {
+                currentValue--;
+            }
+            textBox2.Text = currentValue.ToString();
+        }
+    }
+}
